82cd39dcd193d183172d8778b6f643ce9500180f
yhlu <eric890923@gmail.com>
2025-01-24 01:55:12 -0500

    be able to scraw web page

--- src/main/java/me/juanmacias/HelloLucene.java
+++ src/main/java/me/juanmacias/HelloLucene.java
@@ -16,10 +16,22 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.ByteBuffersDirectory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class HelloLucene {
+
+    private static final String SEED_URL = "https://cc.gatech.edu";
+    private static final int MAX_URLS = 10;
+
+    private static final HashSet<String> visitedUrls = new HashSet<>();
+    private static final Queue<String> urlQueue = new LinkedList<>();
+    private static Directory index;
 
     public static void main(String[] args) throws IOException {
         // 0. Specify the analyzer for tokenizing text.
@@ -27,24 +39,48 @@ public class HelloLucene {
         StandardAnalyzer analyzer = new StandardAnalyzer();
 
         // 1. create the index
-        Directory index = new ByteBuffersDirectory();
+        index = new ByteBuffersDirectory();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
         IndexWriter w = new IndexWriter(index, config);
-        addDoc(w, "Lucene in Action", "193398817");
-        addDoc(w, "Lucene for Dummies", "55320055Z");
-        addDoc(w, "Managing Gigabytes", "55063554A");
-        addDoc(w, "The Art of Computer Science", "9900333X");
+
+        // Initialize URL queue
+        urlQueue.add(SEED_URL);
+
+        while (!urlQueue.isEmpty() && visitedUrls.size() < MAX_URLS) {
+            String url = urlQueue.poll();
+            if (!visitedUrls.contains(url)) {
+                visitedUrls.add(url);
+                System.out.println("Crawling: " + url);
+
+                try {
+                    org.jsoup.nodes.Document htmlDoc = Jsoup.connect(url).get();
+                    String content = htmlDoc.text();
+                    indexDocument(w, url, content);
+
+                    for (Element link : htmlDoc.select("a[href]")) {
+                        String nextUrl = link.absUrl("href");
+                        if (!visitedUrls.contains(nextUrl)) {
+                            urlQueue.add(nextUrl);
+                        }
+                    }
+                } catch (IOException e) {
+                    System.err.println("Failed to fetch URL: " + url);
+                }
+            }
+        }
+
         w.close();
+        System.out.println("Crawling completed. Indexed " + visitedUrls.size() + " URLs.");
 
         // 2. query
-        String queryString = args.length > 0 ? args[0] : "lucene";
+        String queryString = args.length > 0 ? args[0] : "health";
 
         // the "title" arg specifies the default field to use
         // when no field is explicitly specified in the query.
         Query query = null;
         try {
-            query = new QueryParser("title", analyzer).parse(queryString);
+            query = new QueryParser("content", analyzer).parse(queryString);
         } catch (org.apache.lucene.queryparser.classic.ParseException e) {
             e.printStackTrace();
         }
@@ -61,7 +97,7 @@ public class HelloLucene {
         for (int i = 0; i < hits.length; ++i) {
             int docId = hits[i].doc;
             Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+            System.out.println((i + 1) + ". " + d.get("url") + "\t");
         }
 
         // reader can only be closed when there
@@ -77,4 +113,13 @@ public class HelloLucene {
         doc.add(new StringField("isbn", isbn, Field.Store.YES));
         w.addDocument(doc);
     }
+
+    private static void indexDocument(IndexWriter writer, String url, String content) throws IOException {
+        Document doc = new Document();
+        // doc.add(new StringField("url", url, Field.Store.YES));
+        doc.add(new StringField("url", url, Field.Store.YES));
+        doc.add(new TextField("content", content, Field.Store.YES));
+        writer.addDocument(doc);
+    }
+
 }
