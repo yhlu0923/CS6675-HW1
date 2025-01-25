671646cca639061c66c4d9674d7b7ce06256173e
yhlu <eric890923@gmail.com>
2025-01-25 01:40:16 -0500

    update readme and source code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 hello-lucene
 ============
 
+Note: this is the modification code from the below website
 Mavenized version of Kelvin Tan's example <http://www.lucenetutorial.com/lucene-in-5-minutes.html>
 
 Use the following command to run the program:
--- src/main/java/me/juanmacias/HelloLucene.java
+++ src/main/java/me/juanmacias/HelloLucene.java
@@ -27,7 +27,6 @@ import java.util.Queue;
 public class HelloLucene {
 
     private static final String SEED_URL = "https://cc.gatech.edu";
-    private static final int MAX_URLS = 10;
 
     private static final HashSet<String> visitedUrls = new HashSet<>();
     private static final Queue<String> urlQueue = new LinkedList<>();
@@ -36,22 +35,47 @@ public class HelloLucene {
     public static void main(String[] args) throws IOException {
         // 0. Specify the analyzer for tokenizing text.
         // The same analyzer should be used for indexing and searching
-        StandardAnalyzer analyzer = new StandardAnalyzer();
 
         // 1. create the index
         index = new ByteBuffersDirectory();
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-        IndexWriter w = new IndexWriter(index, config);
 
         // Initialize URL queue
         urlQueue.add(SEED_URL);
 
-        while (!urlQueue.isEmpty() && visitedUrls.size() < MAX_URLS) {
+        String[] queryStrings = {"georgia", "page", "new", "Senior", "graduate"};
+
+        System.out.println("Indexed 100 documents.");
+        addMultipleDocuments(100);
+        displayExampleKeywords(queryStrings);
+
+        System.out.println("Indexed 1 documents.");
+        addMultipleDocuments(1);
+        displayExampleKeywords(queryStrings);
+        
+        // 
+        measurePerformance();
+    }
+
+    private static void indexDocument(IndexWriter writer, String url, String content) throws IOException {
+        Document doc = new Document();
+        doc.add(new StringField("url", url, Field.Store.YES));
+        doc.add(new TextField("content", content, Field.Store.YES));
+        writer.addDocument(doc);
+    }
+
+    private static void addMultipleDocuments(int count) throws IOException {
+
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter w = new IndexWriter(index, config);
+
+        int startSize = visitedUrls.size();
+
+        while (!urlQueue.isEmpty() && visitedUrls.size() < startSize + count) {
             String url = urlQueue.poll();
             if (!visitedUrls.contains(url)) {
                 visitedUrls.add(url);
-                System.out.println("Crawling: " + url);
+                // System.out.println("Crawling: " + url);
 
                 try {
                     org.jsoup.nodes.Document htmlDoc = Jsoup.connect(url).get();
@@ -60,44 +84,55 @@ public class HelloLucene {
 
                     for (Element link : htmlDoc.select("a[href]")) {
                         String nextUrl = link.absUrl("href");
-                        if (!visitedUrls.contains(nextUrl)) {
-                            urlQueue.add(nextUrl);
+                        if (nextUrl.startsWith("http://") || nextUrl.startsWith("https://")) {
+                            if (!visitedUrls.contains(nextUrl)) {
+                                urlQueue.add(nextUrl);
+                            }
                         }
                     }
                 } catch (IOException e) {
-                    System.err.println("Failed to fetch URL: " + url);
+                    // System.err.println("Failed to fetch URL: " + url);
                 }
             }
         }
 
         w.close();
-        System.out.println("Crawling completed. Indexed " + visitedUrls.size() + " URLs.");
+        // System.out.println("Crawling completed. Indexed " + visitedUrls.size() + " URLs.");
+    }
 
-        // 2. query
-        String queryString = args.length > 0 ? args[0] : "health";
+    private static void displayExampleKeywords(String[] queryStrings) throws IOException {
 
         // the "title" arg specifies the default field to use
         // when no field is explicitly specified in the query.
-        Query query = null;
-        try {
-            query = new QueryParser("content", analyzer).parse(queryString);
-        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
-            e.printStackTrace();
-        }
 
         // 3. search
-        int hitsPerPage = 10;
+        int hitsPerPage = 1000;
         IndexReader reader = DirectoryReader.open(index);
         IndexSearcher searcher = new IndexSearcher(reader);
-        TopDocs docs = searcher.search(query, hitsPerPage);
-        ScoreDoc[] hits = docs.scoreDocs;
+        
+        for (String queryString : queryStrings) {
+            // long startTime = System.currentTimeMillis();
 
-        // 4. display results
-        System.out.println("Found " + hits.length + " hits.");
-        for (int i = 0; i < hits.length; ++i) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". " + d.get("url") + "\t");
+            Query query = null;
+            try {
+                query = new QueryParser("content", new StandardAnalyzer()).parse(queryString);
+            } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+                e.printStackTrace();
+            }
+    
+            TopDocs docs = searcher.search(query, hitsPerPage);
+            ScoreDoc[] hits = docs.scoreDocs;
+
+            // long endTime = System.currentTimeMillis();
+            
+            // 4. display results
+            System.out.println("Found " + hits.length + " hits." + "\t" + queryString);
+            // System.out.println("The search takes " + (endTime - startTime) + " ms.");
+            // for (int i = 0; i < hits.length; ++i) {
+            //     int docId = hits[i].doc;
+            //     Document d = searcher.doc(docId);
+            //     System.out.println((i + 1) + ". " + d.get("url") + "\t");
+            // }
         }
 
         // reader can only be closed when there
@@ -105,21 +140,16 @@ public class HelloLucene {
         reader.close();
     }
 
-    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
-        Document doc = new Document();
-        doc.add(new TextField("title", title, Field.Store.YES));
+    private static void measurePerformance() throws IOException {
+        String[] queryStrings = {"georgia", "page", "new", "Senior", "graduate"};
+        int[] docCounts = {100, 200, 300};
 
-        // use a string field for isbn because we don't want it tokenized
-        doc.add(new StringField("isbn", isbn, Field.Store.YES));
-        w.addDocument(doc);
+        for (int count : docCounts) {
+            long startTime = System.currentTimeMillis();
+            addMultipleDocuments(count);
+            long endTime = System.currentTimeMillis();
+            System.out.println("Indexed " + count + " documents in " + (endTime - startTime) + " ms.");
+            displayExampleKeywords(queryStrings);
+        }
     }
-
-    private static void indexDocument(IndexWriter writer, String url, String content) throws IOException {
-        Document doc = new Document();
-        // doc.add(new StringField("url", url, Field.Store.YES));
-        doc.add(new StringField("url", url, Field.Store.YES));
-        doc.add(new TextField("content", content, Field.Store.YES));
-        writer.addDocument(doc);
-    }
-
 }
